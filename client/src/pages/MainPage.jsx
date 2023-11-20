d858d726d928c7769214a98677def1e8010ecdc4
Aykut Kav <ayktkav@gmail.com>
2023-11-20 22:11:18 +0100

    repost delete added main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -229,22 +229,6 @@ function MainPage() {
   };
 
   const handleRepost = (postId) => {
-    // const posts = JSON.parse(localStorage.getItem("posts"));
-
-    // const findedPost = posts.find((element) => {
-    //   return element._id === postId;
-    // });
-
-    // const index = posts.indexOf(findedPost);
-    // if (posts[index].reposted.length < 1) {
-    //   posts[index].reposted.unshift(userInfo._id);
-    // } else {
-    //   return;
-    // }
-    // localStorage.setItem("posts", JSON.stringify(posts));
-    // setshouldHide(false);
-    // setPosts(posts);
-
     axios
       .post(
         `${API_URL}/repost`,
@@ -269,6 +253,27 @@ function MainPage() {
         localStorage.setItem("posts", JSON.stringify(posts));
         setshouldHide(false);
         setPosts(posts);
+      })
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleDeleteRepostMainPage = (postId) => {
+    axios
+      .post(
+        `${API_URL}/repost/delete`,
+        { postId: postId, userId: userInfo._id },
+        {
+          headers: {
+            Authorization: `Bearer ${getToken()}`,
+          },
+        }
+      )
+      .then(() => {
+        console.log("");
+        setshouldHide(false);
       })
       .then(() => {})
       .catch((error) => {
@@ -767,8 +772,10 @@ function MainPage() {
                         repostSymbolHandler(posts).includes(userInfo._id) ? (
                           <div>
                             <svg
-                              // onClick={()=> handleDeleteRepost()}
-                              // color="rgb(0, 186, 124)"
+                              onClick={() =>
+                                handleDeleteRepostMainPage(post._id)
+                              }
+                              color="rgb(0, 186, 124)"
                               xmlns="http://www.w3.org/2000/svg"
                               width="16"
                               height="16"
@@ -820,33 +827,6 @@ function MainPage() {
                           </div>
                         )}
                       </div>
-
-                      {/* start to check */}
-                      {/* <div className={`p-0 ${shouldHide ? "hide" : ""}`}>
-                        {post.reposted.length > 0 ? (
-                          <div>
-                            <svg
-                              // onClick={()=> handleDeleteRepost()}
-                              color="rgb(0, 186, 124)"
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              fill="rgb(0, 186, 124)"
-                              className="bi bi-repeat"
-                              viewBox="0 0 16 16"
-                            >
-                              <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192Zm3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z" />
-                            </svg>
-                            <span
-                              style={{ color: "rgb(0, 186, 124)" }}
-                              className="post-description"
-                            >
-                              {post.reposted.length}
-                            </span>
-                          </div>
-                        ) : null}
-                      </div> */}
-                      {/* finish to check */}
 
                       <div className="p-0">
                         {post.likes.includes(userInfo._id) ? (
